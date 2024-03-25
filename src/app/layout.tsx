85e7ee7cfe0c561e9cab88d87cb63eb91035e141
Muhamad Abdul Azis <103145762+muhAzis@users.noreply.github.com>
2024-03-25 16:45:01 +0700

    - Update topography overlay background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,28 @@ export default function RootLayout({
         <link rel="icon" href="/logo-clr.svg" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+
+        <div className="topo-line-container1">
+          <div className="topo-line">
+            <div className="topo-line">
+              <div className="topo-line">
+                <div className="topo-line" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="topo-line-container2">
+          <div className="topo-line">
+            <div className="topo-line">
+              <div className="topo-line">
+                <div className="topo-line" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }

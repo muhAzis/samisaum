2f1e3dd15c3d671900ef56272e2cf4f99892d7f8
Muhamad Abdul Azis <103145762+muhAzis@users.noreply.github.com>
2024-03-27 23:56:56 +0700

    - Update opengraph

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -2,17 +2,10 @@ import React from 'react';
 import { ImageResponse } from 'next/og';
 
 const Image = async () => {
-  return new ImageResponse(
-    (
-      <div style={{ width: 1280, height: 720, backgroundColor: '#4530b3' }}>
-        <img src="/opengraph-image.png" alt="samisaum" />
-      </div>
-    ),
-    {
-      width: 1280,
-      height: 720,
-    }
-  );
+  return new ImageResponse(<div style={{ width: '1280px', height: '720px', backgroundColor: '#4530b3' }}>Samisaum</div>, {
+    width: 1280,
+    height: 720,
+  });
 };
 
 export default Image;
